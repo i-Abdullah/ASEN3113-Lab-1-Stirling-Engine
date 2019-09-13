eaa78081370176abfea9648d7302a93f44ecb78c
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-09-13 15:14:44 -0600

    reads new file

--- Lab1.m
+++ Lab1.m
@@ -15,10 +15,19 @@ close all
 %% read data file
 
 
-
+% from SolidWorks
 FoamDisp = xlsread('Data/Big Lin Disp at CG.xlsx'); % displacement of foam inside controlled volume
 HoleAngularDisp = xlsread('Data/Hole angular Displacement.xlsx'); % angular displacement of the hole on the fly wheel 
 PistonDisp = xlsread('Data/Small Bottom Face Disp.xlsx'); %displacement of Bottom face of the piston.
+
+
+% form Vi: engine was tested with 3 different temprature differences 8 10
+% 12
+
+EightDeg = load('8degrees_engine3');
+TenDeg = load('10degrees_engine3');
+TweleveDeg = load('12degrees_engine3');
+
 
 
 %% RPM : from angular displacement.
