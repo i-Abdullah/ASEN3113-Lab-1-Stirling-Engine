444873245df8e8df880908f86e1f81ea6b1747cc
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-09-13 12:09:55 -0600

    Added new files

--- Lab1.m
+++ Lab1.m
@@ -16,8 +16,20 @@ close all
 
 
 
-FoamDisp = xlsread('/Data/Big Lin Disp at CG.xlsx'); % displacement of foam inside controlled volume
-HoleAngularDisp = xlsread('/Data/Hole angular Displacement.xlsx'); % angular displacement of the hole on the fly wheel 
-PistonDisp = xlsread('/Data/Small Bottom Face Disp.xlsx'); %displacement of Bottom face of the piston.
+FoamDisp = xlsread('Data/Big Lin Disp at CG.xlsx'); % displacement of foam inside controlled volume
+HoleAngularDisp = xlsread('Data/Hole angular Displacement.xlsx'); % angular displacement of the hole on the fly wheel 
+PistonDisp = xlsread('Data/Small Bottom Face Disp.xlsx'); %displacement of Bottom face of the piston.
 
+
+%% RPM : from angular displacement.
+
+% Get RPM from hole angular displacement.
+
+Angle = HoleAngularDisp(:,3);
+Angle = Angle*(pi/180); % convert from deg to radian
+
+%RPM = ( Angle ./ (HoleAngularDisp(:,2)/60)) ./ (2*pi) ;
+
+
+%% RPM : From linear displacement
 
