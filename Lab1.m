0695362b74a44be3447d08778cd706395938b2bc
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-10-08 14:54:58 -0600

    Added the results for the other 2 cases

--- Lab1.m
+++ Lab1.m
@@ -43,19 +43,34 @@ PistonDisp = xlsread('Data/Small Bottom Face Disp.xlsx'); %displacement of Botto
 %FoamDisp_ = xlsread('Data/RPM_Data/Big Lin Disp at CG.xlsx'); % displacement of foam inside controlled volume
 %HoleAngularDisp = xlsread('Data/RPM_Data/Hole angular Displacement.xlsx'); % angular displacement of the hole on the fly wheel 
 PistonDisp_8 = xlsread('Data/RPM_Data/SmallBottom96.xlsx'); %displacement of Bottom face of the piston.
+PistonDisp_10 = xlsread('Data/RPM_Data/SmallBottom112.xlsx'); %displacement of Bottom face of the piston.
+PistonDisp_12 = xlsread('Data/RPM_Data/SmallBottom129.xlsx'); %displacement of Bottom face of the piston.
 
 % zero data:
 
 PistonDisp_8(:,3) = PistonDisp_8(:,3) - min(PistonDisp_8(:,3));
+PistonDisp_10(:,3) = PistonDisp_10(:,3) - min(PistonDisp_10(:,3));
+PistonDisp_12(:,3) = PistonDisp_12(:,3) - min(PistonDisp_12(:,3));
 
 % make sure that we start from displacement = 0:
 
 j = find(PistonDisp_8(:,3)==min(PistonDisp_8(:,3))); % 
 PistonDisp_8(1:j-1,:) = [];
 
+j = find(PistonDisp_10(:,3)==min(PistonDisp_10(:,3))); % 
+PistonDisp_10(1:j-1,:) = [];
+
+j = find(PistonDisp_12(:,3)==min(PistonDisp_12(:,3))); % 
+PistonDisp_12(1:j-1,:) = [];
+
+
 % zero time
 
 PistonDisp_8(:,2) = PistonDisp_8(:,2) - (PistonDisp_8(1,2));
+
+PistonDisp_10(:,2) = PistonDisp_10(:,2) - (PistonDisp_10(1,2));
+
+PistonDisp_12(:,2) = PistonDisp_12(:,2) - (PistonDisp_12(1,2));
 
 
 % form Vi: engine was tested with 3 different temprature differences
@@ -194,22 +209,52 @@ End_t_12 = T12(T12_Pass(find_T12(2),1)) - T12(T12_Pass(find_T12(1),1));
 
 i = knnsearch(PistonDisp_8(:,2),End_t_8); % find where times are the same.
 
-
 Volume_8 = V1 + PistonDisp_8(1:i,3)*10^-3 * (pi*(RPiston*10^-3)^2) ;
 t_Volume_8 = PistonDisp_8(1:i,2);
+
+
+i = knnsearch(PistonDisp_10(:,2),End_t_10); % find where times are the same.
+i = i + 200 ;
+Volume_10 = V1 + PistonDisp_10(1:i,3)*10^-3 * (pi*(RPiston*10^-3)^2) ;
+t_Volume_10 = PistonDisp_10(1:i,2);
+
+
+i = knnsearch(PistonDisp_12(:,2),End_t_12); % find where times are the same.
+i = i + 200 ;
+Volume_12 = V1 + PistonDisp_12(1:i,3)*10^-3 * (pi*(RPiston*10^-3)^2) ;
+t_Volume_12 = PistonDisp_12(1:i,2);
+
 
 
 Pressure_8 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),2)  ; 
 Pressure_8 = Pressure_8 + 12 ; % add atmospheric pressur in psi
 Pressure_8 = Pressure_8* 6894.76 ; % convert to Pa
-Pressure_8 = Pressure_8;
 
+Pressure_10 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),2)  ; 
+Pressure_10 = Pressure_10 + 12 ; % add atmospheric pressur in psi
+Pressure_10 = Pressure_10* 6894.76 ; % convert to Pa
+
+
+Pressure_12 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),2)  ; 
+Pressure_12 = Pressure_12 + 12 ; % add atmospheric pressur in psi
+Pressure_12 = Pressure_12* 6894.76 ; % convert to Pa
 
 
 t_Pressure_8 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),1) ;
 t_Pressure_8 = t_Pressure_8 - t_Pressure_8(1) ; 
 Volume_8_interp = interp1(t_Volume_8,Volume_8,t_Pressure_8);
 
+t_Pressure_10 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),1) ;
+t_Pressure_10 = t_Pressure_10 - t_Pressure_10(1) ; 
+Volume_10_interp = interp1(t_Volume_10,Volume_10,t_Pressure_10);
+
+
+t_Pressure_12 = T8(T8_Pass(find_T8(1)):T8_Pass(find_T8(2)),1) ;
+t_Pressure_12 = t_Pressure_12 - t_Pressure_12(1) ; 
+Volume_12_interp = interp1(t_Volume_12,Volume_12,t_Pressure_12);
+
+
+figure(1)
 scatter(Volume_8_interp,Pressure_8)
 grid minor
 xlabel('Volume [m^3]');
@@ -217,7 +262,23 @@ ylabel('Pressure [Pa]');
 title(['PV diagram for T = 8' char(176) ' C stirling engine'])
 
  
+figure(2)
+scatter(Volume_10_interp,Pressure_10)
+grid minor
+xlabel('Volume [m^3]');
+ylabel('Pressure [Pa]');
+title(['PV diagram for T = 10' char(176) ' C stirling engine'])
+
+
+figure(3)
+scatter(Volume_12_interp,Pressure_12)
+grid minor
+xlabel('Volume [m^3]');
+ylabel('Pressure [Pa]');
+title(['PV diagram for T = 12' char(176) ' C stirling engine'])
+
 hi = 0;
+
 
 
 
@@ -257,12 +318,26 @@ m_air = 9;
 W12_8 = 0;
 Qnet_12_8 = m_air*Cv_air*((8)) ;
 
+W12_10 = 0;
+Qnet_12_10 = m_air*Cv_air*((10)) ;
+
+W12_12 = 0;
+Qnet_12_12 = m_air*Cv_air*((12)) ;
+
+
 % -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 % 2 -> 3 :
 
 W23_8 = m_air*R_air*(max(( T8(:,4) + T8(:,5))./2)+273.15)*log(V2/V1) ;
 Qnet_23_8 = W23_8 ;
+
+W23_10 = m_air*R_air*(max(( T10(:,4) + T10(:,5))./2)+273.15)*log(V2/V1) ;
+Qnet_23_10 = W23_10 ;
+
+W23_12 = m_air*R_air*(max(( T12(:,4) + T12(:,5))./2)+273.15)*log(V2/V1) ;
+Qnet_23_12 = W23_12 ;
+
 
 % -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -271,12 +346,25 @@ Qnet_23_8 = W23_8 ;
 W34_8 = 0 ;
 Qnet_34_8 = -m_air*Cv_air*((8)) ;
 
+W34_10 = 0 ;
+Qnet_34_10 = -m_air*Cv_air*((10)) ;
+
+W34_12 = 0 ;
+Qnet_34_12 = -m_air*Cv_air*((12)) ;
+
+
 % -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 % 4 -> 1 :
 
 W41_8 =  m_air*R_air*(min(( T8(:,4) + T8(:,5))./2)+273.15)*log(V1/V2);
 Qnet_41_8 = W41_8 ;
+
+W41_10 =  m_air*R_air*(min(( T10(:,4) + T10(:,5))./2)+273.15)*log(V1/V2);
+Qnet_41_10 = W41_10 ;
+
+W41_12 =  m_air*R_air*(min(( T12(:,4) + T12(:,5))./2)+273.15)*log(V1/V2);
+Qnet_41_12 = W41_12 ;
 
 
 % themral efficiency: 
@@ -285,20 +373,36 @@ Qnet_41_8 = W41_8 ;
 
 % ideal efficiency from stilring
 
-T1 = (min(( T8(:,4) + T8(:,5))./2)+273.15);
-T2 = T1;
+T1_8 = (min(( T8(:,4) + T8(:,5))./2)+273.15);
+T2_8 = T1_8;
+T3_8 = (max(( T8(:,4) + T8(:,5))./2)+273.15);
+T4_8 = T3_8;
 
-T3 = (max(( T8(:,4) + T8(:,5))./2)+273.15);
-T4 = T3;
+T1_10 = (min(( T10(:,4) + T10(:,5))./2)+273.15);
+T2_10 = T1_10;
+T3_10 = (max(( T10(:,4) + T10(:,5))./2)+273.15);
+T4_10 = T3_10;
+
+T1_12 = (min(( T12(:,4) + T12(:,5))./2)+273.15);
+T2_12 = T1_12;
+T3_12 = (max(( T12(:,4) + T12(:,5))./2)+273.15);
+T4_12 = T3_12;
 
 
 
+nth_ideal_8 = ( T3_8 - T1_8 ) / ( T3_8 + (( Cv_Air *( T3_8 - T2_8 ) ) / R_air*log(V2/V1) ) ) * 100 ;
+nth_actual_8 = ( trapz(Volume_8_interp(1:1020),Pressure_8(1:1020)) ./ (Qnet_12_8+Qnet_23_8) ) * 100  ;
 
-nth_ideal = ( T3 - T1 ) / ( T3 + (( Cv_Air *( T3 - T2 ) ) / R_air*log(V2/V1) ) ) * 100 
-nth_actual = ( trapz(Volume_8_interp(1:1020),Pressure_8(1:1020)) ./ (Qnet_12_8+Qnet_23_8) ) * 100  
+nth_ideal_10 = ( T3_10 - T1_10 ) / ( T3_10 + (( Cv_Air *( T3_10 - T2_10 ) ) / R_air*log(V2/V1) ) ) * 100 ;
+nth_actual_10 = ( trapz(Volume_10_interp(1:1020),Pressure_10(1:1020)) ./ (Qnet_12_10+Qnet_23_10) ) * 100  ;
 
-%% actual stirling engine:
 
-% using pressure data from experimental and volume from Solidworks model:
+nth_ideal_12 = ( T3_12 - T1_12 ) / ( T3_12 + (( Cv_Air *( T3_12 - T2_12 ) ) / R_air*log(V2/V1) ) ) * 100 ;
+nth_actual_12 = ( trapz(Volume_12_interp(1:1020),Pressure_12(1:1020)) ./ (Qnet_12_12+Qnet_23_12) ) * 100  ;
 
-%scatter([linspace(V1,V2,length(w))],[
+%% output results:
+
+Actual = { nth_actual_8 ; nth_actual_10 ; nth_actual_12 } ;
+Ideal = { nth_ideal_8 ; nth_ideal_10 ; nth_ideal_12 } ;
+Results = { 'T = 8';'T = 10';'T = 12'};
+table(Results,Actual,Ideal)
